e6ec9c3ca859e5d6c068b864a35040fac7ced2e7
May <xTema05x@mail.ru>
2024-03-13 11:12:35 +0300

    fix

--- HTML and CSS/script.js
+++ HTML and CSS/script.js
@@ -1,14 +1,15 @@
 const boxs = document.querySelector(".color_boxs");
 
+boxs.style.display = "flex";
+boxs.style.gap = "2px";
+boxs.style.flexWrap = "wrap";
+boxs.style.margin = "0 0 30px 0";
+
 for (let i = 1; i <= 100; i++) {
     let boxDiv = document.createElement("div");
     boxDiv.textContent = i;
     boxDiv.classList.add("number-" + i);
     boxDiv.style.width = "50px";
     boxDiv.style.height = "50px";
-    boxs.style.display = "flex";
-    boxs.style.gap = "2px";
-    boxs.style.flexWrap = "wrap";
-    boxs.style.margin = "0 0 30px 0";
     boxs.appendChild(boxDiv);
 }
