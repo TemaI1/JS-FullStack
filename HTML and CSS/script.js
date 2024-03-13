6831d54ef5f0151bdd8832588f852aca3e38be94
May <xTema05x@mail.ru>
2024-03-13 11:00:19 +0300

    html, scss

---
+++ HTML and CSS/script.js
@@ -0,0 +1,14 @@
+const boxs = document.querySelector(".color_boxs");
+
+for (let i = 1; i <= 100; i++) {
+    let boxDiv = document.createElement("div");
+    boxDiv.textContent = i;
+    boxDiv.classList.add("number-" + i);
+    boxDiv.style.width = "50px";
+    boxDiv.style.height = "50px";
+    boxs.style.display = "flex";
+    boxs.style.gap = "2px";
+    boxs.style.flexWrap = "wrap";
+    boxs.style.margin = "0 0 30px 0";
+    boxs.appendChild(boxDiv);
+}
